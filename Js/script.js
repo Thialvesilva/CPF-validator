ebc915383278c4d6f214349baa329d74584cb39b
Thiago Alves <Thialvesilva7@gmail.com>
2026-08-31 16:23:19 -0300

    ajuste

--- Js/script.js
+++ Js/script.js
@@ -1,15 +1,32 @@
-const cpf  = document.getElementById('cpf');
+const cpf = document.getElementById('cpf');
 const validar = document.getElementById('validar');
 const resultado = document.getElementById('resultado');
 
 function validarCPF() {
     const cpfDigitado = cpf.value
-    const cpfLImpo = cpfDigitado.replace(/[.-]/g, "")
-    if (cpfLImpo.length !== 11){
+    const cpfLimpo = cpfDigitado.replace(/[.-]/g, "")
+    if (cpfLimpo.length !== 11) {
         resultado.textContent = "CPF inválido"
-    }else{
-        resuladto.textContent = "CPF Valido"
+    } else {
+        resultado.textContent = "CPF Válido"
+        const primeiros9 = cpfLimpo.slice(0, 9);
+        let soma = 0;
+        for (let i = 0; i < primeiros9.length; i++) {
+            primeiros9[i]
+            Pesos[i]
+            let multiplicacao = primeiros9[i] * Pesos[i]
+            soma += multiplicacao;
+        }
+        let resto = soma % 11;
+        let primeiroDigito;
+
+        if (resto <= 1) {
+           primeiroDigito = 0
+        } else {
+           primeiroDigito = 11 - resto
+        }
+        const primeiroDigitoCPF = cpfLimpo[9];
     }
 }
-
+const Pesos = [10, 9, 8, 7, 6, 5, 4, 3, 2]
 validar.addEventListener('click', validarCPF)
